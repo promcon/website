27ef9fee1d450ae464db8aef17d37a3f137a325c
Tobias Schmidt <tobidt@gmail.com>
2016-09-13 21:26:01 -0400

    Allow fullscreen of embedded players

--- lib/helpers/embed_players.rb
+++ lib/helpers/embed_players.rb
@@ -1,6 +1,6 @@
 def youtube_player(id)
   url = "https://www.youtube.com/embed/#{id}?list=PLoz-W_CUquUlCq-Q0hy53TolAhaED9vm&origin=https://promcon.io"
-  container %(<iframe id="ytplayer" type="text/html" src="#{url}" frameborder="0"></iframe></div>)
+  container %(<iframe id="ytplayer" type="text/html" src="#{url}" allowFullScreen="true" frameborder="0"></iframe></div>)
 end
 
 def slideshare_player(code)
@@ -12,11 +12,11 @@ def google_slides_player(id)
 end
 
 def google_drive_player(id)
-  container %(<iframe src="https://drive.google.com/file/d/#{id}/preview"></iframe>)
+  container %(<iframe src="https://drive.google.com/file/d/#{id}/preview" allowFullScreen="true"></iframe>)
 end
 
 def speakerdeck_player(id)
-  container %(<script async class="speakerdeck-embed" data-id="#{id}" data-ratio="1.77777777777778" src="//speakerdeck.com/assets/embed.js"></script>)
+  container %(<script async class="speakerdeck-embed" data-id="#{id}" data-ratio="1.77777777777778" src="//speakerdeck.com/assets/embed.js" allowFullScreen="true"></script>)
 end
 
 def container(content)
