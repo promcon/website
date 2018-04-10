3ed0bd65fbe3e00004bc237804b3cd4b9b0b6bdd
Richard Hartmann <richih@richih.org>
2018-04-10 23:23:55 +0200

    Add linkage to sponsor page now that page is good & merged

--- lib/helpers/conferences.rb
+++ lib/helpers/conferences.rb
@@ -49,6 +49,7 @@ def conferences
        '/2018-munich/' => 'Overview',
        '/2018-munich/submit/' => 'Call for Papers',
 #       '/2018-munich/schedule/' => 'Schedule',
+       '/2018-munich/sponsor/' => 'Sponsor',
        '/coc/' => 'Code of Conduct',
       },
       Banner.new('munich-banner', 'Qwrt!', 'https://www.flickr.com/photos/qwertworks/9983260225/'),
