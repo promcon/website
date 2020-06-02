bd860c17007ba4908d75e26e0fb34c3ba4de2d71
Brian Brazil <brian.brazil@robustperception.io>
2020-06-02 16:08:13 +0100

    Close CfP
    Signed-off-by: Brian Brazil <brian.brazil@robustperception.io>

--- lib/helpers/conferences.rb
+++ lib/helpers/conferences.rb
@@ -84,7 +84,7 @@ def conferences
 #       '/2020-vancouver/register/' => 'Register',
 #       '/2020-vancouver/diversity/' => 'Diversity',
         '/2020-vancouver/covid-19/' => 'COVID-19 Notice',
-        '/2020-vancouver/submit/' => 'Submit Talk',
+#        '/2020-vancouver/submit/' => 'Submit Talk',
 #       '/2020-vancouver/schedule/' => 'Schedule',
         '/2020-vancouver/sponsor/' => 'Sponsor',
 #       '/2020-vancouver/stream' => 'Live Stream',
